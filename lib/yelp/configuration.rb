164156eea72e39f3ae831d505de29d6cdefa6d6c
Ali Jepsen <ali.jepsen@gmail.com>
2018-04-20 16:30:57 -0600

    keys

--- lib/yelp/configuration.rb
+++ lib/yelp/configuration.rb
@@ -1,6 +1,6 @@
 module Yelp
   class Configuration
-    AUTH_KEYS = [:fusion_key]
+    AUTH_KEYS = [:fusion_key, :consumer_key, :consumer_secret, :token, :token_secret]
 
     attr_accessor *AUTH_KEYS
 
