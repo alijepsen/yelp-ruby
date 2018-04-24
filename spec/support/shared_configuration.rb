bd3ab527330236fbbe09e195816a52e6b55eab3e
Ali Jepsen <ali.jepsen@gmail.com>
2018-04-24 16:58:58 -0600

    more tries

--- spec/support/shared_configuration.rb
+++ spec/support/shared_configuration.rb
@@ -3,11 +3,11 @@ shared_context "shared configuration" do
                           consumer_secret: 'def',
                           token: 'ghi',
                           token_secret: 'jkl',
-                          fusion_key: 'mno'] }
+                          api_key: 'mno'] }
   let(:real_api_keys) { Hash[consumer_key: ENV['YELP_CONSUMER_KEY'],
                     consumer_secret: ENV['YELP_CONSUMER_SECRET'],
                     token: ENV['YELP_TOKEN'],
                     token_secret: ENV['YELP_TOKEN_SECRET']]
-                    fusion_key: ENV['YELP_FUSION_KEY']] }
+                    api_key: ENV['YELP_FUSION_KEY']] }
   let(:invalid_api_keys) { valid_api_keys.merge(consumer_key: nil) }
 end
